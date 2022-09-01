99d6de205f355b54ab8b28b28e29439271e8c771
CleilsonAndrade <cleilsonjose@hotmail.com>
2022-09-01 00:31:06 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('#image-picker li')
+const image = document.querySelector('#product-image')
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log(e)
+
+    buttons.forEach((btn) => {
+      btn.querySelector('.color').classList.remove('selected')
+    })
+
+    const button = e.target
+    console.log(e.target)
+    
+    const id = button.getAttribute('id')
+    console.log(id)
+
+    button.querySelector('.color').classList.add('selected')
+
+    image.classList.toggle('changing')
+    image.setAttribute('src', `img/iphone_${id}.jpg`)
+
+    setTimeout(() => {
+      image.classList.toggle('changing')
+    }, 200)
+  })
+})
